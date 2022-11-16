0a94642f883324b27f7302106534d7458db5f3f6
Juanfh11 <juanfh2003@gmail.com>
2022-11-16 12:50:21 +0100

    Añadido Activity del total de faltas

---
+++ app/src/main/java/com/example/simp_2/TotalActivity.java
@@ -0,0 +1,14 @@
+package com.example.simp_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TotalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_total);
+    }
+}
